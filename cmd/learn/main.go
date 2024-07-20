8a8eb506a1c156daef46b8f6b329ef8e0a65f7a0
gucio321 <73652197+gucio321@users.noreply.github.com>
2024-07-20 17:14:17 +0200

    move tutorial to pkg/

--- cmd/learn/main.go
+++ cmd/learn/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
-	"github.com/gucio32/morse/pkg/generator"
 	"github.com/gucio32/morse/pkg/learn"
 )
 
@@ -17,49 +15,7 @@ func main() {
 	lesson := learn.GetLesson(*lessonIdx)
 
 	if *tutor {
-		// print letters in this lesson and their morse code
-		for _, letter := range lesson.Letters {
-			code, valid := generator.TranslateMorse(rune(letter))
-			if !valid {
-				panic("Not a valid letter")
-			}
-
-			fmt.Printf("%s: %s\n", string(rune(letter)), code)
-		}
-
-		var answer string
-		fmt.Print("Type letter to hear it or enter to exit: ")
-		gen, err := generator.NewGenerator()
-		if err != nil {
-			panic(err)
-		}
-
-		for fmt.Scanln(&answer); answer != ""; fmt.Scanln(&answer) {
-			// check if onnly 1 letters
-			if len(answer) != 1 {
-				fmt.Println("Only one letter allowed")
-				continue
-			}
-
-			// check if letter is in lesson
-			c := false
-			for _, letter := range lesson.Letters {
-				if answer[0] == byte(letter) {
-					c = true
-					break
-				}
-			}
-
-			if !c {
-				fmt.Println("Not in lesson")
-				continue
-			}
-
-			gen.Play(string(rune(answer[0])))
-
-			fmt.Print("Type letter to hear it or enter to exit: ")
-		}
-
+		learn.Tutorial(lesson)
 		return
 	}
 
--- pkg/learn/learn.go
+++ pkg/learn/learn.go
@@ -15,17 +15,20 @@ import (
 // According to https://morsecode.world/international/timing.html
 // it is recommended to work on PARIS=20 and increase InterChar and InterWord breaks
 type Lesson struct {
-	Letters   string
-	InterChar int
-	InterWord int
+	Letters     string
+	InterChar   int
+	InterWord   int
+	Description string
 }
 
 func GetLesson(lessonIdx int) Lesson {
 	m := map[int]Lesson{
-		1: {"aelv", 9, 21},
-		2: {"aelv", 6, 14},
-		3: {"aelvcqst", 9, 21},
-		4: {"aelvcqst", 6, 14},
+		1: {"aelv", 9, 21, "First lesson: a, e, l, v higher inter-words and inter-characters breaks"},
+		2: {"aelv", 6, 14, "Second lesson: a, e, l, v medium inter-words and inter-characters breaks"},
+		3: {"aelvcqst", 9, 21, "Third lesson: a, e, l, v, c, q, s, t higher inter-words and inter-characters breaks"},
+		4: {"aelvcqst", 6, 14, "Fourth lesson: a, e, l, v, c, q, s, t medium inter-words and inter-characters breaks"},
+		5: {"aelvcqstfgny", 9, 21, "Fifth lesson: a, e, l, v, c, q, s, t, f, g, n, y higher inter-words and inter-characters breaks"},
+		6: {"aelvcqstfgny", 6, 14, "Sixth lesson: a, e, l, v, c, q, s, t, f, g, n, y medium inter-words and inter-characters breaks"},
 	}
 
 	l, ok := m[lessonIdx]
@@ -34,6 +37,51 @@ func GetLesson(lessonIdx int) Lesson {
 	}
 
 	return l
+}
+
+func Tutorial(lesson Lesson) {
+	// print letters in this lesson and their morse code
+	for _, letter := range lesson.Letters {
+		code, valid := generator.TranslateMorse(rune(letter))
+		if !valid {
+			panic("Not a valid letter")
+		}
+
+		fmt.Printf("%s: %s\n", string(rune(letter)), code)
+	}
+
+	var answer string
+	fmt.Print("Type letter to hear it or enter to exit: ")
+	gen, err := generator.NewGenerator()
+	if err != nil {
+		panic(err)
+	}
+
+	for fmt.Scanln(&answer); answer != ""; fmt.Scanln(&answer) {
+		// check if onnly 1 letters
+		if len(answer) != 1 {
+			fmt.Println("Only one letter allowed")
+			ncontinue
+		}
+
+		// check if letter is in lesson
+		c := false
+		for _, letter := range lesson.Letters {
+			if answer[0] == byte(letter) {
+				c = true
+				break
+			}
+		}
+
+		if !c {
+			fmt.Println("Not in lesson")
+			continue
+		}
+
+		gen.Play(string(rune(answer[0])))
+
+		fmt.Print("Type letter to hear it or enter to exit: ")
+	}
 }
 
 func StartLesson(l Lesson, nWords int) {
