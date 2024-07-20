98af3228e552bffca69ed5887c14721d864eab50
gucio321 <73652197+gucio321@users.noreply.github.com>
2024-07-20 09:37:45 +0200

    generator: remove stuff related to another oto formats

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -202,7 +202,7 @@ type SineWave struct {
 }
 
 func NewSineWave(freq float64, duration time.Duration, channelCount int, format oto.Format) *SineWave {
-	l := int64(DefaultChannelCount) * int64(formatByteLength(format)) * int64(DefaultSampleRate) * int64(duration) / int64(time.Second)
+	l := int64(DefaultChannelCount) * int64(formatByteLength()) * int64(DefaultSampleRate) * int64(duration) / int64(time.Second)
 	l = l / 4 * 4
 	return &SineWave{
 		freq:         freq,
@@ -240,44 +240,21 @@ func (s *SineWave) Read(buf []byte) (int, error) {
 
 	length := float64(DefaultSampleRate) / float64(s.freq)
 
-	num := formatByteLength(s.format) * s.channelCount
+	num := formatByteLength() * s.channelCount
 	p := s.pos / int64(num)
-	switch s.format {
-	case oto.FormatFloat32LE:
-		for i := 0; i < len(buf)/num; i++ {
-			bs := math.Float32bits(float32(math.Sin(2*math.Pi*float64(p)/length) * 0.3))
-			for ch := 0; ch < DefaultChannelCount; ch++ {
-				buf[num*i+4*ch] = byte(bs)
-				buf[num*i+1+4*ch] = byte(bs >> 8)
-				buf[num*i+2+4*ch] = byte(bs >> 16)
-				buf[num*i+3+4*ch] = byte(bs >> 24)
-			}
-			p++
-		}
-	case oto.FormatUnsignedInt8:
-		for i := 0; i < len(buf)/num; i++ {
-			const max = 127
-			b := int(math.Sin(2*math.Pi*float64(p)/length) * 0.3 * max)
-			for ch := 0; ch < DefaultChannelCount; ch++ {
-				buf[num*i+ch] = byte(b + 128)
-			}
-			p++
-		}
-	case oto.FormatSignedInt16LE:
-		for i := 0; i < len(buf)/num; i++ {
-			const max = 32767
-			b := int16(math.Sin(2*math.Pi*float64(p)/length) * 0.3 * max)
+	for i := 0; i < len(buf)/num; i++ {
+		const max = 32767
+		b := int16(math.Sin(2*math.Pi*float64(p)/length) * 0.3 * max)
 
-			if p <= stuckReduction {
-				b = int16(float64(p) / (math.Pi / 2 * stuckReduction) * math.Sin(2*math.Pi*float64(p)/length) * 0.3 * max)
-			}
-
-			for ch := 0; ch < DefaultChannelCount; ch++ {
-				buf[num*i+2*ch] = byte(b)
-				buf[num*i+1+2*ch] = byte(b >> 8)
-			}
-			p++
+		if p <= stuckReduction {
+			b = int16(float64(p) / (math.Pi / 2 * stuckReduction) * math.Sin(2*math.Pi*float64(p)/length) * 0.3 * max)
 		}
+
+		for ch := 0; ch < DefaultChannelCount; ch++ {
+			buf[num*i+2*ch] = byte(b)
+			buf[num*i+1+2*ch] = byte(b >> 8)
+		}
+		p++
 	}
 
 	s.pos += int64(len(buf))
@@ -309,15 +286,6 @@ func (s *SineWave) Seek(offset int64, whence int) (int64, error) {
 	return s.pos, nil
 }
 
-func formatByteLength(format oto.Format) int {
-	switch format {
-	case oto.FormatFloat32LE:
-		return 4
-	case oto.FormatUnsignedInt8:
-		return 1
-	case oto.FormatSignedInt16LE:
-		return 2
-	default:
-		panic(fmt.Sprintf("unexpected format: %d", format))
-	}
+func formatByteLength() int {
+	return 2
 }
